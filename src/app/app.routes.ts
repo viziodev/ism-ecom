9faa7d121d1a7ff91aa6be0d5c780a95f0ad56f6
douvewane <douvewane85@gmail.com>
2024-03-27 03:15:44 +0000

    Child Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,23 @@ import { Routes } from '@angular/router';
 import { ClientsComponent } from './core/pages/clients/clients.component';
 import { CommandesComponent } from './core/pages/commandes/commandes.component';
 import { PageNotFoundComponent } from './core/pages/page-not-found/page-not-found.component';
+import { FormClientComponent } from './core/pages/clients/form.client/form.client.component';
 
 export const routes: Routes = [
      {
       path:"clients",
-      component:ClientsComponent
+         children:[
+            {
+               path:"",
+               component:ClientsComponent,
+            },
+            {
+               path:"form",
+               component:FormClientComponent
+            }
+         ]
      },
+     
      {
       path:"commandes/:id",
       component:CommandesComponent
--- src/app/core/pages/clients/clients.component.ts
+++ src/app/core/pages/clients/clients.component.ts
@@ -3,12 +3,12 @@ import { RestResponse } from '../../models/rest.response';
 import { ClientListe } from '../../models/client.liste';
 import { ClientServiceImpl } from '../../services/impl/client.service.impl';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-clients',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink,RouterOutlet],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css'
 })
